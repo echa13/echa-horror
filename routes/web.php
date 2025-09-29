e626abd93081847ed43cd2acece9b60dd79cb6ea
salsabila24si <salsabila24si@mahasiswa.pcr.ac.id>
2025-09-29 10:11:21 +0700

    Membuat Form Pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 
 
@@ -33,3 +34,5 @@ Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/Pegawai', [PegawaiController::class, 'index']);
 
+Route::post('question/store', [QuestionController::class, 'store'])
+->name('question.store');
