6c61e77c299c7c1631232c8713b73806e9507aed
salsabila24si <salsabila24si@mahasiswa.pcr.ac.id>
2025-10-13 10:56:33 +0700

    penambahan untuk inputan pelanggan

--- resources/views/admin/pelanggan/create.blade.php
+++ resources/views/admin/pelanggan/create.blade.php
@@ -294,7 +294,8 @@
                                 <div class="list-group list-group-flush">
                                     <a href="#"
                                         class="text-center text-primary fw-bold border-bottom border-light py-3">Notifications</a>
-                                    <a href="#" class="list-group-item list-group-item-action border-bottom">
+                                    <a href="#"
+                                        class="list-group-item list-group-item-pelanggan.store border-bottom">
                                         <div class="row align-items-center">
                                             <div class="col-auto">
                                                 <!-- Avatar -->
@@ -516,43 +517,57 @@
         <div class="row">
             <div class="col-12 mb-4">
                 <div class="card border-0 shadow">
+
                     <div class="card-header bg-primary text-white py-3">
                         <h5 class="mb-0">Formulir Tambah Pelanggan Baru</h5>
                     </div>
+
                     <div class="card-body">
-                        <form>
+                        @if (session('success'))
+                            <div class="alert alert-info">
+                                {!! session('success') !!}
+                            </div>
+                        @endif
+                        </div>
+                        <form action="{{ route('pelanggan.store') }}" method="POST">
+                            @csrf
+
                             <div class="row mb-4">
+
                                 <div class="col-lg-6">
                                     <h6 class="mb-3 text-dark">Data Diri</h6>
 
                                     <div class="mb-4">
                                         <label for="firstName" class="form-label">Nama Depan <span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" id="firstName" name="first_name"
-                                            placeholder="Masukkan Nama Depan" required>
+                                        <input type="text"
+                                            class="form-control @error('first_name') is-invalid @enderror"
+                                            id="firstName" name="first_name" placeholder="Masukkan Nama Depan"
+                                            value="{{ old('first_name') }}" required>
+                                        @error('first_name')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
 
                                     <div class="mb-4">
                                         <label for="lastName" class="form-label">Nama Belakang</label>
-                                        <input type="text" class="form-control" id="lastName" name="last_name"
-                                            placeholder="Masukkan Nama Belakang">
+                                        <input type="text"
+                                            class="form-control @error('last_name') is-invalid @enderror"
+                                            id="lastName" name="last_name" placeholder="Masukkan Nama Belakang"
+                                            value="{{ old('last_name') }}">
+                                        @error('last_name')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
 
-                                    <div class="mb-4">
-                                        <label for="birthday" class="form-label">Tanggal Lahir</label>
-                                        <div class="input-group">
-                                            <span class="input-group-text">
-                                                <svg class="icon icon-xs text-gray-600" fill="currentColor"
-                                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                    <path fill-rule="evenodd"
-                                                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                        clip-rule="evenodd"></path>
-                                                </svg>
-                                            </span>
-                                            <input data-datepicker="" class="form-control" id="birthday"
-                                                name="birthday" type="text" placeholder="dd/mm/yyyy">
-                                        </div>
-                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                    <label for="birthday" class="form-label">Birthday</label>
+                                    <input type="date" class="form-control @error('birthday') is-invalid @enderror"
+                                        id="birthday" name="birthday" value="{{ old('birthday') }}" required>
+                                    @error('birthday')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
                                 </div>
 
@@ -562,35 +577,55 @@
                                     <div class="mb-4">
                                         <label for="emailInput" class="form-label">Email <span
                                                 class="text-danger">*</span></label>
-                                        <input type="email" class="form-control" id="emailInput" name="email"
-                                            placeholder="contoh@email.com" required>
+                                        <input type="email"
+                                            class="form-control @error('email') is-invalid @enderror" id="emailInput"
+                                            name="email" placeholder="contoh@email.com"
+                                            value="{{ old('email') }}" required>
                                         <small id="emailHelp" class="form-text text-muted">Email harus aktif dan
                                             valid.</small>
+                                        @error('email')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
 
                                     <div class="mb-4">
                                         <label for="phoneInput" class="form-label">Nomor Telepon</label>
-                                        <input type="tel" class="form-control" id="phoneInput" name="phone"
-                                            placeholder="Cth: 0812XXXXXXXX">
+                                        <input type="tel"
+                                            class="form-control @error('phone') is-invalid @enderror" id="phoneInput"
+                                            name="phone" placeholder="Cth: 0812XXXXXXXX"
+                                            value="{{ old('phone') }}">
+                                        @error('phone')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
 
                                     <div class="mb-4">
                                         <label class="d-block form-label">Jenis Kelamin</label>
+                                        @php
+                                            $oldGender = old('gender');
+                                        @endphp
+
                                         <div class="form-check form-check-inline">
                                             <input class="form-check-input" type="radio" name="gender"
-                                                id="genderMale" value="Male" required>
+                                                id="genderMale" value="Male"
+                                                @if ($oldGender == 'Male') checked @endif>
                                             <label class="form-check-label" for="genderMale">Laki-laki</label>
                                         </div>
                                         <div class="form-check form-check-inline">
                                             <input class="form-check-input" type="radio" name="gender"
-                                                id="genderFemale" value="Female">
+                                                id="genderFemale" value="Female"
+                                                @if ($oldGender == 'Female') checked @endif>
                                             <label class="form-check-label" for="genderFemale">Perempuan</label>
                                         </div>
                                         <div class="form-check form-check-inline">
                                             <input class="form-check-input" type="radio" name="gender"
-                                                id="genderOther" value="Other">
+                                                id="genderOther" value="Other"
+                                                @if ($oldGender == 'Other') checked @endif>
                                             <label class="form-check-label" for="genderOther">Lainnya</label>
                                         </div>
+                                        @error('gender')
+                                            <div class="text-danger small mt-1">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
